15b1fbf591f6fd9749476f30fda864ec69328748
Dmitriy <46672460+Ru5D1mG4me@users.noreply.github.com>
2023-01-21 17:29:45 +0000

    Introduction of protected variables and initial values for classes

--- main.py
+++ main.py
@@ -1,73 +1,73 @@
 class Dec():
-  def __init__(self, dec_num):
-    self.dec_num = dec_num
-    self.num = self.dec_num
+  def __init__(self, dec_num:int=0):
+    self._dec_num = dec_num
+    self._num = self._dec_num
     
   def __repr__(self):
-    return 'Dec: ' + self.num
+    return 'Dec: ' + self._num
     
   def to_dec(self):
-    return str(self.dec_num)
+    return str(self._dec_num)
     
   def get_num(self):
-    return self.num
+    return self._num
     
   def __add__(self, other):
     if self.__class__.__name__ == 'Bin':
-      return Bin(self.dec_num + other.dec_num)
+      return Bin(self._dec_num + other._dec_num)
     elif self.__class__.__name__ == 'Oct':
-      return Oct(self.dec_num + other.dec_num)
+      return Oct(self._dec_num + other._dec_num)
     elif self.__class__.__name__ == 'Hex':
-      return Hex(self.dec_num + other.dec_num)
+      return Hex(self._dec_num + other._dec_num)
       
   def __sub__(self, other):
     if self.__class__.__name__ == 'Bin':
-      return Bin(self.dec_num - other.dec_num)
+      return Bin(self._dec_num - other._dec_num)
     elif self.__class__.__name__ == 'Oct':
-      return Oct(self.dec_num - other.dec_num)
+      return Oct(self._dec_num - other._dec_num)
     elif self.__class__.__name__ == 'Hex':
-      return Hex(self.dec_num - other.dec_num)
+      return Hex(self._dec_num - other._dec_num)
 
   def __mul__(self, other):
     if self.__class__.__name__ == 'Bin':
-      return Bin(self.dec_num * other.dec_num)
+      return Bin(self._dec_num * other._dec_num)
     elif self.__class__.__name__ == 'Oct':
-      return Oct(self.dec_num * other.dec_num)
+      return Oct(self._dec_num * other._dec_num)
     elif self.__class__.__name__ == 'Hex':
-      return Hex(self.dec_num * other.dec_num)
+      return Hex(self._dec_num * other._dec_num)
     
   def __truediv__(self, other):
     if self.__class__.__name__ == 'Bin':
-      return Bin(self.dec_num // other.dec_num)
+      return Bin(self._dec_num // other._dec_num)
     elif self.__class__.__name__ == 'Oct':
-      return Oct(self.dec_num // other.dec_num)
+      return Oct(self._dec_num // other._dec_num)
     elif self.__class__.__name__ == 'Hex':
-      return Hex(self.dec_num // other.dec_num)
+      return Hex(self._dec_num // other._dec_num)
 
     
 class Bin(Dec):
-  def __init__(self, dec_num):
+  def __init__(self, dec_num:int=0):
     super().__init__(dec_num)
-    self.num = bin(self.dec_num).replace('0b', '')
+    self._num = bin(self._dec_num).replace('0b', '')
     
   def __repr__(self):
-    return f'Bin: {self.num}\nDec: {self.dec_num}'
+    return f'Bin: {self._num}\nDec: {self._dec_num}'
 
 
 
 class Oct(Dec):
-  def __init__(self, dec_num):
+  def __init__(self, dec_num:int=0):
     super().__init__(dec_num)
-    self.num = oct(self.dec_num).replace('0o', '')
+    self._num = oct(self._dec_num).replace('0o', '')
     
   def __repr__(self):
-    return f'Oct: {self.num}\nDec: {self.dec_num}'
+    return f'Oct: {self._num}\nDec: {self._dec_num}'
 
 
 class Hex(Dec):
-  def __init__(self, dec_num):
+  def __init__(self, dec_num:int=0):
     super().__init__(dec_num)
-    self.num = hex(self.dec_num).replace('0x', '').upper()
+    self._num = hex(self._dec_num).replace('0x', '').upper()
     
   def __repr__(self):
-    return f'Hex: {self.num}\nDec: {self.dec_num}'
+    return f'Hex: {self._num}\nDec: {self._dec_num}'
